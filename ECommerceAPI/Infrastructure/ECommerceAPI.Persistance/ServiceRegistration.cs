f46081a76ebd5f5c572209a9c4a821a0046694f8
Emre Tetik <emre.tetik@elekse.com>
2023-04-07 01:56:43 +0300

    add layers with productscontroller

---
+++ ECommerceAPI/Infrastructure/ECommerceAPI.Persistance/ServiceRegistration.cs
@@ -0,0 +1,20 @@
+﻿using ECommerceAPI.Application.Abstractions;
+using ECommerceAPI.Persistance.Concretes;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceAPI.Persistance
+{
+    public static class ServiceRegistration
+    {
+        public static void AddPersistanceServices(this IServiceCollection services)
+        {
+            services.AddSingleton<IProductService, ProductService>();
+        }
+    }
+}

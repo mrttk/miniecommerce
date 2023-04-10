bb1c9a838715678f688d27d8f88c34b426c7da42
Emre Tetik <emre.tetik@elekse.com>
2023-04-10 15:08:31 +0300

    Add tracking options for performance

--- ECommerceAPI/Core/ECommerceAPI.Application/Repositories/IReadRepository.cs
+++ ECommerceAPI/Core/ECommerceAPI.Application/Repositories/IReadRepository.cs
@@ -10,9 +10,9 @@ namespace ECommerceAPI.Application.Repositories
 {
     public interface IReadRepository<TEntity> : IRepository<TEntity> where TEntity : BaseEntity
     {
-        IQueryable<TEntity> GetAll();
-        IQueryable<TEntity> GetWhere(Expression<Func<TEntity, bool>> method);
-        Task<TEntity> GetSingleAsync(Expression<Func<TEntity, bool>> method);
-        Task<TEntity> GetByIdAsync(string id);
+        IQueryable<TEntity> GetAll(bool tracking = true);
+        IQueryable<TEntity> GetWhere(Expression<Func<TEntity, bool>> method, bool tracking = true);
+        Task<TEntity> GetSingleAsync(Expression<Func<TEntity, bool>> method, bool tracking = true);
+        Task<TEntity> GetByIdAsync(string id, bool tracking = true);
     }
 }
--- ECommerceAPI/Infrastructure/ECommerceAPI.Persistance/Repositories/ReadRepository.cs
+++ ECommerceAPI/Infrastructure/ECommerceAPI.Persistance/Repositories/ReadRepository.cs
@@ -22,12 +22,33 @@ namespace ECommerceAPI.Persistance.Repositories
 
         public DbSet<TEntity> Table => _context.Set<TEntity>();
 
-        public IQueryable<TEntity> GetAll() => Table;
-
-        public IQueryable<TEntity> GetWhere(Expression<Func<TEntity, bool>> method) => Table.Where(method);
-
-        public async Task<TEntity> GetSingleAsync(Expression<Func<TEntity, bool>> method) => await Table.FirstOrDefaultAsync(method);
-        public async Task<TEntity> GetByIdAsync(string id) => await Table.FindAsync(Guid.Parse(id));
-
+        public IQueryable<TEntity> GetAll(bool tracking = true)
+        {
+            var query = Table.AsQueryable();
+            if (!tracking)
+                query = query.AsNoTracking();
+            return query;
+        }
+        public IQueryable<TEntity> GetWhere(Expression<Func<TEntity, bool>> method, bool tracking = true)
+        {
+            var query = Table.Where(method);
+            if (!tracking)
+                query = query.AsNoTracking();
+            return query;
+        }
+        public async Task<TEntity> GetSingleAsync(Expression<Func<TEntity, bool>> method, bool tracking = true)
+        {
+            var query = Table.AsQueryable();
+            if (!tracking)
+                query = query.AsNoTracking();
+            return await query.FirstOrDefaultAsync(method);
+        }
+        public async Task<TEntity> GetByIdAsync(string id, bool tracking = true)
+        {
+            var query = Table.AsQueryable();
+            if (!tracking)
+                query = query.AsNoTracking();
+            return await Table.FindAsync(Guid.Parse(id));
+        }
     }
 }

763db0fd038994875e766f153a728bf588b3a142
Emre Tetik <emre.tetik@elekse.com>
2023-05-15 21:02:40 +0300

    add ngx-spinner package

--- ECommerceClient/src/app/admin/components/orders/orders.module.ts
+++ ECommerceClient/src/app/admin/components/orders/orders.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { OrdersComponent } from './orders.component';
 import { RouterModule } from '@angular/router';
+import { NgxSpinnerModule } from 'ngx-spinner';
 
 @NgModule({
   declarations: [OrdersComponent],
   imports: [
     CommonModule,
-    RouterModule.forChild([{ path: "", component: OrdersComponent }]),
+    RouterModule.forChild([{ path: '', component: OrdersComponent }]),
   ],
 })
 export class OrdersModule {}
